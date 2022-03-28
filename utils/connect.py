8c7e3a11ce15102eda484f8be347f272fd483ffd
Dyllon <dyllon.g@outlook.com>
2022-03-28 20:27:46 +0800

    Downgrade to python 3.8

--- utils/connect.py
+++ utils/connect.py
@@ -4,11 +4,10 @@ from utils.get_credentials import get_credentials
 
 def connect_to_db():
     credentials = get_credentials()
-    # todo: connect via credentials
-    print(credentials)
+    # print(credentials)
 
-    db_name = ""
-    conn = pymssql.connect(credentials['db']['url'], credentials['db']['user'], credentials['db']['password'], db_name)
-    cursor = conn.cursor()
-    conn.close()
+    # db_name = ""
+    # conn = pymssql.connect(credentials['db']['url'], credentials['db']['user'], credentials['db']['password'], db_name)
+    # cursor = conn.cursor()
+    # conn.close()
     return  # return a db instance (maybe a cursor???)
